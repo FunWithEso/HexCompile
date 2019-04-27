55bd8ae7100e2544632c68ce7a638e50f7f331b9
ayitsparker <pmlannom@vt.edu>
2019-04-27 17:30:46 -0400

    Added hex.l with all language tokens

--- parse.cpp
+++ parse.cpp
@@ -1,4 +1,5 @@
 #include "parse.h"
+#include <fstream>
 
 Parser::Parser(std::string filename){
 	this->filename = filename;
@@ -10,4 +11,15 @@ Parser::~Parser(){
 
 void Parser::ParseFile(){
 	//Start actual token parsing....
+
+	string line;
+  	ifstream myfile (filename);
+  	if (myfile.is_open())
+  	{
+    	while ( getline (myfile,line) )
+    	{
+      		cout << line << '\n';
+    	}
+    	myfile.close();
+  	}
 }
